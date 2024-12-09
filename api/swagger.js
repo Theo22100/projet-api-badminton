57c968bb186126958731834ce58b5ece0cce1e7e
QuillG <quentin.guillemand@gmail.com>
2024-12-10 00:59:56 +0100

    fix authorize config for bearer token

--- api/swagger.js
+++ api/swagger.js
@@ -7,51 +7,45 @@ const endpointsFiles = ['./app.js'];
 
 //@See: https://swagger-autogen.github.io/docs/getting-started/
 const doc = {
-    info: {
-      version: '3.0.0',            
-      title: 'Badminton RESTful API', 
-      description: ''     
+  info: {
+    version: '3.0.0',
+    title: 'Badminton RESTful API',
+    description: 'Documentation pour l\'API de badminton',
+  },
+  host: 'localhost:5001',
+  basePath: '',
+  schemes: ['http'], // Utilisez 'https' si votre API est sécurisée
+  securityDefinitions: {
+    BearerAuth: {
+      type: 'apiKey', // Swagger 2.0 utilise `apiKey` pour représenter un Bearer Token
+      name: 'Authorization', // Le header attendu pour le token
+      in: 'header', // Le token sera envoyé dans le header
+      description: 'Entrez "Bearer <votre-token>" pour vous authentifier.',
     },
-    host: 'localhost:5001',                 // by default: 'localhost:3000'
-    basePath: '',             // by default: '/'
-    schemes: ['http'],
-    components: {
-      securitySchemes: {
-        BearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
+  },
+  security: [
+    {
+      BearerAuth: [],
     },
-    security: [
-      {
-        BearerAuth: [],
-      },
-    ],              // by default: ['http']
-    consumes: [],             // by default: ['application/json']
-    produces: [],             // by default: ['application/json']
-    tags: [ 
-      {
-        name: 'Users',
-        description: 'Routes liées aux utilisateurs'
-      },
-      {
-        name: 'Terrains',
-        description: 'Routes liées aux Terrains'
-      },
-      {
-        name: 'Réservations',
-        description: 'Routes liées aux réservations'
-      },
-      {
-        name: 'Home',
-        description: 'Routes liées à la page d\'accueil ou au point de base de l\'API'
-      }
-    ],
-    securityDefinitions: {},  // by default: empty object
-    definitions: {}           // by default: empty object
-  };
-  
+  ],
+  tags: [
+    {
+      name: 'Users',
+      description: 'Routes liées aux utilisateurs',
+    },
+    {
+      name: 'Terrains',
+      description: 'Routes liées aux Terrains',
+    },
+    {
+      name: 'Réservations',
+      description: 'Routes liées aux réservations',
+    },
+    {
+      name: 'Home',
+      description: 'Routes liées à la page d\'accueil ou au point de base de l\'API',
+    },
+  ],
+};
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
