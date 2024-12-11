3987257e08ef2e7767f2c7ec821a54ffe747cb3b
Theo22100 <theoguerinBZH@hotmail.com>
2024-12-11 19:53:05 +0100

    fix error 500

--- api/routes/reservations.js
+++ api/routes/reservations.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
         const reservations = await Reservation.findAll();
         res.status(200).json(await mapReservationListToRessourceObject(reservations));
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
 
 });
